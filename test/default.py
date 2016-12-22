44184ca613a5b1546b0a696a784bccabcfd15016
Yota Toyama <raviqqe@gmail.com>
2016-12-22 21:32:06 +0900

    Add default.py test script

---
+++ test/default.py
@@ -0,0 +1,7 @@
+import gargparse
+
+
+gargparse.add_argument('-f', '--foo',
+                       type=str, default="bar",
+                       help="This is an example.")
+print(gargparse.ARGS.foo)
