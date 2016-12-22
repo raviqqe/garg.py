5a2fa3c4bcf7c02f16e201416e19094364bf40bc
Yota Toyama <raviqqe@gmail.com>
2016-12-22 21:28:05 +0900

    Follow PEP8

--- gargparse.py
+++ gargparse.py
@@ -1,25 +1,26 @@
 import argparse
 
 
-
 _PARSER = argparse.ArgumentParser()
 _ARGS = None
 
+
 class _Args:
-  def __getattr__(self, name):
-    global _ARGS
 
-    if _ARGS == None:
-      _ARGS = parse_args()
+    def __getattr__(self, name):
+        global _ARGS
 
-    return getattr(_ARGS, name)
+        if _ARGS == None:
+            _ARGS = parse_args()
+
+        return getattr(_ARGS, name)
 
 ARGS = _Args()
 
 
 def add_argument(*args, **kwargs):
-  _PARSER.add_argument(*args, **kwargs)
+    _PARSER.add_argument(*args, **kwargs)
 
 
 def parse_args(*args, **kwargs):
-  return _PARSER.parse_args(*args, **kwargs)
+    return _PARSER.parse_args(*args, **kwargs)
--- setup.py
+++ setup.py
@@ -6,33 +6,33 @@ import sys
 
 
 if not sys.version_info[0] >= 3:
-  exit("Sorry, Python's version must be later than 3.")
+    exit("Sorry, Python's version must be later than 3.")
 
 
 def main():
-  shutil.copy("README.rst", "README")
+    shutil.copy("README.rst", "README")
 
-  with open("README.rst") as f:
-    readme = f.read()
+    with open("README.rst") as f:
+        readme = f.read()
 
-  setuptools.setup(
-    name="gargparse",
-    version="0.0.4",
-    description="Global argparse parser",
-    long_description=readme,
-    license="Public Domain",
-    author="Yota Toyama",
-    author_email="raviqqe@gmail.com",
-    url="http://github.com/raviqqe/gargparse.py/",
-    py_modules=["gargparse"],
-    classifiers=[
-      "Development Status :: 3 - Alpha",
-      "Intended Audience :: Developers",
-      "License :: Public Domain",
-      "Programming Language :: Python :: 3",
-    ],
-  )
+    setuptools.setup(
+        name="gargparse",
+        version="0.0.4",
+        description="Global argparse parser",
+        long_description=readme,
+        license="Public Domain",
+        author="Yota Toyama",
+        author_email="raviqqe@gmail.com",
+        url="http://github.com/raviqqe/gargparse.py/",
+        py_modules=["gargparse"],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "License :: Public Domain",
+            "Programming Language :: Python :: 3",
+        ],
+    )
 
 
 if __name__ == "__main__":
-  main()
+    main()
