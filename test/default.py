40eaa4cc905ee1c55b7b2732302869995102bc7f
Yota Toyama <raviqqe@gmail.com>
2016-12-24 20:24:57 +0900

    Prefer " to '

--- test/default.py
+++ test/default.py
@@ -1,7 +1,7 @@
 import gargparse
 
 
-gargparse.add_argument('-f', '--foo',
+gargparse.add_argument("-f", "--foo",
                        type=str, default="bar",
                        help="This is an example.")
 print(gargparse.ARGS.foo)
--- test/simple.py
+++ test/simple.py
@@ -1,5 +1,5 @@
 import gargparse
 
 
-gargparse.add_argument('foo', type=str, help="This is an example.")
+gargparse.add_argument("foo", type=str, help="This is an example.")
 print(gargparse.ARGS.foo)
